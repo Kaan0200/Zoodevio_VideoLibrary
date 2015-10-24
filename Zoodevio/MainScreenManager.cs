961540953229c496ac0e0346048b75b714a4ae74
Harry Nelken <hrn10@case.edu>
2015-10-24 16:56:09 -0400

    most of search and map algorithm worked out
    Signed-off-by: Harry Nelken <hrn10@case.edu>

--- Zoodevio/MainScreenManager.cs
+++ Zoodevio/MainScreenManager.cs
@@ -9,17 +9,59 @@ namespace Zoodevio
 {
     class MainScreenManager
     {
+        // This class sets the directory at the given URL as the Zoodevio library root
         public void SetLibraryRoot(string rootURL)
         {
+            // If this directory exists, begin mapping file locations
             DirectoryInfo root = new DirectoryInfo(rootURL);
-            DirectoryInfo subRoot = root;
-            DirectoryInfo[] subDirs = root.GetDirectories("*.*", System.IO.SearchOption.AllDirectories);
-            
-            while (subDirs.Length > 0)
+            if (root.Exists()) {
+                ClearPreviousRoot();
+                SearchAndMapDirectory(root);
+            }
+        }
+
+        // This digs into all of the given directory's subdirectories mapping file and directory locations.
+        private void SearchAndMapDirectory(DirectoryInfo subDir)
+        {
+            MapDirectoryLocation(subDir);
+            DirectoryInfo[] subDirs = subDir.GetDirectories("*.*", System.IO.SearchOption.TopDirectoryOnly);
+
+            for (int i = 0; i < subDirs.Length; i++)
+            {
+                DirectoryInfo subRoot = subDirs[i];
+                SearchAndMapDirectory(subRoot);
+            }
+
+            string[] extensions = GetSupportedFileExtensions();
+            for (int i = 0; i < extensions.Length; i++)
             {
 
             }
-            root.GetFiles();
+            FileInfo[] videoFiles = subDir.GetFiles("*.mp4");
+            for (int i = 0; i < videoFiles.Length; i++)
+            {
+                MapFileLocation(videoFiles[i], subDir);
+            }
+        }
+
+        private string[] GetSupportedFileExtensions()
+        {
+            // TODO: Get an array of supported file extensions in "xxx" format (no '.')
+        }
+
+        private void ClearPreviousRoot()
+        {
+            // TODO: Tell the database to remove its reference to any existing library root
+        }
+
+        private void MapDirectoryLocation(DirectoryInfo dir)
+        {
+            // TODO: Add this directory to the database as a folder object
+        }
+
+        private void MapFileLocation(FileInfo videoFile, DirectoryInfo parentDir)
+        {
+            // TODO: Add this file to the database under the specified parent folder
         }
 
     }
