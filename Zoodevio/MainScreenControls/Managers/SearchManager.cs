3d640d711e3684116b1516634dc9e495995d7d8c
Kaan Atesoglu <aka43@case.edu>
2015-10-24 16:15:43 -0400

    Added basic search manager

--- Zoodevio/MainScreenControls/Managers/SearchManager.cs
+++ Zoodevio/MainScreenControls/Managers/SearchManager.cs
@@ -1,5 +1,8 @@
 ﻿namespace Zoodevio.Managers
 {
+    /* This manager is for the basic search controller and for the advanced search controller,
+    this will not need to be modified for the first release. */
+
     public class SearchManager
     {
          
