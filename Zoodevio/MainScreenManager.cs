e1756ee7483267cfbc837ac2472f685acc1254bc
Harry Nelken <hrn10@case.edu>
2015-10-26 18:51:40 -0400

    finished root setting traversal algorithm without connect to DB
    Signed-off-by: Harry Nelken <hrn10@case.edu>

--- Zoodevio/MainScreenManager.cs
+++ Zoodevio/MainScreenManager.cs
@@ -15,54 +15,90 @@ namespace Zoodevio
             // If this directory exists, begin mapping file locations
             DirectoryInfo root = new DirectoryInfo(rootURL);
             if (root.Exists()) {
-                ClearPreviousRoot();
-                SearchAndMapDirectory(root);
+                // Clear old root and set new root
+                SetRootReference(root);
+                
+                // Traverse this directory mapping all file/folder locations
+                MapDirectoryContents(root);
             }
         }
 
         // This digs into all of the given directory's subdirectories mapping file and directory locations.
-        private void SearchAndMapDirectory(DirectoryInfo subDir)
+        private void MapDirectoryContents(DirectoryInfo subDir)
         {
-            MapDirectoryLocation(subDir);
-            DirectoryInfo[] subDirs = subDir.GetDirectories("*.*", System.IO.SearchOption.TopDirectoryOnly);
+            // Find all video files in this folder
+            MapAllContainedFileLocations(subDir);
 
-            for (int i = 0; i < subDirs.Length; i++)
+            // Get all immediately contained subdirectories
+            DirectoryInfo[] subDirs = GetImmediateSubDirectories(subDir);
+            
+            // For each subdirectory:
+            for (int i = 0; subDirs != null && i < subDirs.Length; i++)
             {
-                DirectoryInfo subRoot = subDirs[i];
-                SearchAndMapDirectory(subRoot);
-            }
+                // Map its location in the database
+                MapDirectoryLocation(subDirs[i]);
 
-            string[] extensions = GetSupportedFileExtensions();
-            for (int i = 0; i < extensions.Length; i++)
-            {
-
-            }
-            FileInfo[] videoFiles = subDir.GetFiles("*.mp4");
-            for (int i = 0; i < videoFiles.Length; i++)
-            {
-                MapFileLocation(videoFiles[i], subDir);
+                // Continue the library traversal 
+                MapDirectoryContents(subDirs[i]);
             }
         }
 
+        // Get the top level subdirectories in a diven directory if possible
+        private DirectoryInfo[] GetImmediateSubDirectories(DirectoryInfo dir)
+        {
+            try
+            {  // Try to get all top level sub directories in this folder
+                return dir.GetDirectories("*", System.IO.SearchOption.TopDirectoryOnly);
+            } catch (Exception e)
+            {    // Something went wrong with permissions or nonexistent references
+                Console.WriteLine("Error getting subdirectories, not sure how to handle yet");
+                return new DirectoryInfo[0];
+            }
+        }
+
+        // Gets an array of strings containing all supported file extensions
         private string[] GetSupportedFileExtensions()
         {
             // TODO: Get an array of supported file extensions in "xxx" format (no '.')
         }
 
-        private void ClearPreviousRoot()
+        // Set new library root reference to the given directory
+        private void SetRootReference(DirectoryInfo root)
         {
             // TODO: Tell the database to remove its reference to any existing library root
+            // TODO: Set the new root reference to the given directory
         }
 
+        // Adds a directory in the library structure as a "Folder" object in the database
         private void MapDirectoryLocation(DirectoryInfo dir)
         {
             // TODO: Add this directory to the database as a folder object
         }
 
-        private void MapFileLocation(FileInfo videoFile, DirectoryInfo parentDir)
+        // Adds a supported video file to the library as a "File" object in the database
+        private void MapFileLocation(FileInfo videoFile, String extension, DirectoryInfo parentDir)
         {
             // TODO: Add this file to the database under the specified parent folder
         }
 
+        // This maps the location of every supported video file in a given directory
+        private void MapAllContainedFileLocations(DirectoryInfo subDir)
+        {
+            // Get all supported file extensions
+            string[] extensions = GetSupportedFileExtensions();
+
+            // For each supported extension:
+            for (int i = 0; i < extensions.Length; i++)
+            {
+                // Get all contained video files of the same type
+                FileInfo[] videoFiles = subDir.GetFiles("*." + extensions[i]);
+
+                // For each video file with the current extension:
+                for (int j = 0; j < videoFiles.Length; j++)
+                {   // Map its location in the database
+                    MapFileLocation(videoFiles[j], extensions[i], subDir);
+                }
+            }
+        }
     }
 }
