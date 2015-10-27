252ea046675ae629994f863a968b73ffbc86e985
max <zimon@case.edu>
2015-10-27 13:49:47 -0400

    add preferences object; shell for preferences DB class

--- Zoodevio/DataModel/Objects/Preference.cs
+++ Zoodevio/DataModel/Objects/Preference.cs
@@ -1,7 +1,24 @@
-﻿namespace Zoodevio.DataModel.Objects
+﻿/*
+ * Represents a preference selection.
+ * For example, defaulting to grid or library view.
+ */
+
+namespace Zoodevio.DataModel.Objects
 {
     public class Preference
     {
-         //test 
+        // the preference's id -- set by database
+        public int Id { get;  }
+
+        // the preference's name
+        public string Name; 
+
+        // the preference's data type in the DB
+        // represented as a string because this is 
+        // just about how to parse text data
+        public string DataType;
+
+        // the preference data
+        public string Data; 
     }
 }
--- Zoodevio/DataModel/Tables/Preferences.cs
+++ Zoodevio/DataModel/Tables/Preferences.cs
@@ -1,7 +1,32 @@
-﻿namespace Zoodevio.DataModel
+﻿/*
+ * Accessing and writing to preferences in the Zoodevio database.
+ * Note that preferences are programmatic and never added/deleted,
+ * only examined and modified. 
+ */
+
+using System;
+using Zoodevio.DataModel.Objects;
+
+namespace Zoodevio.DataModel
 {
     public static class Preferences
     {
-         
+        private static string _table = "preferences"; 
+    
+        public Preference Lookup(int id)
+        {
+            return null;
+        }
+
+        public Preference Lookup(string name)
+        {
+            return null;
+        }
+
+        public Boolean Modify(int id)
+        {
+            return null;
+        }
+
     }
 }
