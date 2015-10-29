fdb00e9ae9ea7e13f5a2f6a12d0ad96b1a78a12c
andrewMarmorstein <agm67@case.edu>
2015-10-29 04:59:20 -0400

    wrote a test for simple update
    didn't write any other tests for the database class because i couldn't
    get insert to work so i couldn't test the other methods.

--- Zoodevio/DataModel/Tests/Database_Test.cs
+++ Zoodevio/DataModel/Tests/Database_Test.cs
@@ -10,13 +10,20 @@ namespace Zoodevio.DataModel.Tests
     {
 
         [Test]
-        public void Database_SimpleInsertQuary_Accept()
+        public void Database_SimpleInsertQuery_Accept()
         {
             var rows = new String[] {"id", "path", "date_added", "date_edited"};
             var data = new String[] {"1", "path", "now", "now"};
             Assert.True(Database.SimpleInsertQuery("files", rows, data));
         }
 
+        [Test]
+        public void Database_SimpleUpdateQuery_Accept()
+        {
+            var rows = new String[] { "id", "path", "date_added", "date_edited" };
+            var data = new String[] { "1", "path", "now", "now" };
+            Assert.True(Database.SimpleUpdateQuery("files", "1", 1, rows, data));
+        }
 
     }
 }
