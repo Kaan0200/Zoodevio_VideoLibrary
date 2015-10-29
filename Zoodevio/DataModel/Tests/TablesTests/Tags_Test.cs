d3ada03666596a8a8f80e066c85bc1e6435969f4
andrewMarmorstein <agm67@case.edu>
2015-10-28 22:59:27 -0400

    added a class for testing tags

---
+++ Zoodevio/DataModel/Tests/TablesTests/Tags_Test.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Data;
+using NUnit.Framework;
+using Zoodevio.DataModel.Objects;
+
+namespace Zoodevio.DataModel.Tests.TablesTests
+{
+    [TestFixture]
+    class Tags_Test
+    {
+
+        [Test]
+        public void Tags_GetFileTags_Accept()
+        {
+            
+        }
+    }
+}
