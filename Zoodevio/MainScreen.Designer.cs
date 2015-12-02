71954a6986721c9cb4b4afd9af147e66b6815f50
max <zimon@case.edu>
2015-12-02 13:53:51 -0500

    remove errant button

--- Zoodevio/MainScreen.Designer.cs
+++ Zoodevio/MainScreen.Designer.cs
@@ -265,16 +265,6 @@
             this.metadataViewControl.TabIndex = 0;
             this.metadataViewControl.Load += new System.EventHandler(this.metadataViewControl_Load);
             // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(0, 0);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 3;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
             // MainScreen
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
